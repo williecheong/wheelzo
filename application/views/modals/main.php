43d61c41c4ca4a8b27c69c79b87c5ec3b76daa69
williecheong <cheongwillie@hotmail.com>
2014-04-09 00:27:16 -0700

    form for creating rides (needs styles)

--- application/views/modals/main.php
+++ application/views/modals/main.php
@@ -9,7 +9,44 @@
                 </h4>
             </div>
             <div class="modal-body"> 
-                Test
+                <form class="form-horizontal" role="form">  
+                    <div class="form-group">  
+                        <label class="col-sm-3 control-label" for="input1">DriverID</label>  
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="id" id="input1" placeholder="">    
+                        </div>
+                    </div> 
+                    <div class="form-group">  
+                        <label class="col-sm-3 control-label" for="input2">To</label>  
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="to" id="input2" placeholder="">  
+                        </div>
+                    </div>  
+                    <div class="form-group">  
+                        <label class="col-sm-3 control-label" for="input3">From</label>  
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="from" id="input3" rows="8" placeholder="">  
+                        </div>
+                    </div>  
+                    <div class="form-group">  
+                        <label class="col-sm-3 control-label" for="input4">When</label>  
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="when" id="input4" rows="8" placeholder="">    
+                        </div>
+                    </div>
+                    <div class="form-group">  
+                        <label class="col-sm-3 control-label" for="input5">Price</label>  
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="price" id="input5" rows="8" placeholder="">  
+                        </div>
+                    </div> 
+                    <div class="form-group">  
+                        <label class="col-sm-3 control-label" for="input6">Capacity</label>  
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="capacity" id="input6" rows="8" placeholder="">  
+                        </div>
+                    </div> 
+                </form>  
             </div>
         </div>
     </div>
