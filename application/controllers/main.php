8538d128a2991f380d0000cb144d4e26be0e72db
williecheong <cheongwillie@hotmail.com>
2014-04-13 01:31:41 -0700

    Facebook in the constructor

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,7 +4,7 @@ class Main extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-        // Referenced from:
+        // Facebook login referenced from:
         //  http://phpguidance.wordpress.com/2013/09/27/facebook-login-with-codeignator/comment-page-1/
         parse_str($_SERVER['QUERY_STRING'],$_REQUEST);
         $this->load->library('Facebook', 
@@ -13,10 +13,31 @@ class Main extends CI_Controller {
                 "secret" => FB_SECRET
             )
         );
-
+        
+        $this->facebook_url = '';    
         $this->facebook_user = $this->facebook->getUser();
         
-        // Autoloaded Config, Helpers, Models
+        if ( $this->facebook_user ) {
+            // Registers the facebook user if not already done.
+            // Always returns the local user ID of this person from our database.
+            $user_id = $this->user->try_register( $this->facebook_user );
+            
+            $this->session->set_userdata('user_id', $user_id);
+            
+            $this->facebook_url = $this->facebook->getLogouturl(
+                array(
+                    "next" => base_url() . 'api/misc/logout'
+                )
+            );
+        
+        } else {
+            $this->facebook_url = $this->facebook->getLoginUrl(
+                array(
+                    "scope" => "email",
+                    "display" => "page"
+                )
+            );
+        }
     }
     
 	public function index() {
@@ -48,33 +69,12 @@ class Main extends CI_Controller {
             $temp_users[$user->id]['facebook_id'] = $user->facebook_id;
         }
 
-        $url = '';
-        if ( $this->facebook_user ) {
-            // Registers the facebook user if not already done.
-            // Always returns the local user ID of this person from our database.
-            $user_id = $this->user->try_register( $this->facebook_user );
-            $this->session->set_userdata('user_id', $user_id);
-            
-            $url = $this->facebook->getLogouturl(
-                array(
-                    "next" => base_url() . 'api/misc/logout'
-                )
-            );
-        } else {
-            $url = $this->facebook->getLoginUrl(
-                array(
-                    "scope" => "email",
-                    "display" => "page"
-                )
-            );
-        }
-
         $this->blade->render('main', 
             array(
                 'rides' => $temp_rides,
                 'users' => $temp_users,
                 'session' => $this->session->userdata('user_id'),
-                'session_url' => $url 
+                'session_url' => $this->facebook_url
             )
         );
     }
