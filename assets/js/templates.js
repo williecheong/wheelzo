e4e3b0564e6aa9e215684a4cb81b3d3406135826
williecheong <cheongwillie@hotmail.com>
2014-04-15 18:06:26 -0700

    Fun passenger picture sizes

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -4,19 +4,28 @@
     function passengersTemplate( rideID ) {
         var html = '';
         var capacity = parseInt( rides[rideID].capacity );
-        var colSize = Math.floor( 12 / capacity );
+        var colSizes = [];
+        if      ( capacity == 1 ) { colSizes = [12]; } 
+        else if ( capacity == 2 ) { colSizes = [6,6]; } 
+        else if ( capacity == 3 ) { colSizes = [4,4,4]; } 
+        else if ( capacity == 4 ) { colSizes = [3,3,3,3]; } 
+        else if ( capacity == 5 ) { colSizes = [4,4,4,6,6]; } 
+        else if ( capacity == 6 ) { colSizes = [4,4,4,4,4,4]; } 
+        else if ( capacity == 7 ) { colSizes = [3,3,3,3,4,4,4]; }
+        else { for(var i=0; i < capacity; i++) colSizes[i] = 2; }
+
         var count = 0 ;
 
         $.each(rides[rideID].passengers, function(key, value){
             var passenger = publicUsers[value.user_id];
-            html += '<div class="col-xs-'+colSize+'">'+
+            html += '<div class="col-xs-'+colSizes[count]+'">'+
                     '    <img class="img-circle" id="passenger-picture" src="//graph.facebook.com/'+passenger.facebook_id+'/picture?width=200&height=200">'+
                     '</div>';
             count++;
         });
 
         while ( count < capacity ) {
-            html += '<div class="col-xs-'+colSize+'">'+
+            html += '<div class="col-xs-'+colSizes[count]+'">'+
                     '    <img class="img-circle image-faded" id="passenger-picture" src="/assets/img/empty_user.png">'+
                     '</div>';
             count++;
