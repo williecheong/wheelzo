fe887a19bd1edef9f11bab7d9558efd7ff4222a6
williecheong <cheongwillie@hotmail.com>
2014-04-19 19:53:01 -0700

    dynamic baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://wheelzo.dev';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/production/config.php
+++ application/config/production/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://wheelzo.com';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
