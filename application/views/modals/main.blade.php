cab52a612cfb91d1a9c4c236c8c5ef5504286d8c
Maksym "Max" Pikhteryev <maksym.p@gmail.com>
2014-04-26 15:46:55 -0700

    header text for x button

--- application/views/modals/main.blade.php
+++ application/views/modals/main.blade.php
@@ -66,8 +66,10 @@
 <div class="modal fade" id="view-ride" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-body"> 
+            <div class="modal-header">
                 <button type="button" class="close visible-xs" data-dismiss="modal" aria-hidden="true">&times;</button>
+            </div>
+            <div class="modal-body"> 
                 <div class="row">
                     <div class="col-sm-4 text-center">
                         <p>
