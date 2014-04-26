c2e03b17e7f5a131d15f60f97405a749f4037b11
Maksym "Max" Pikhteryev <maksym.p@gmail.com>
2014-04-26 15:57:18 -0700

    no border

--- application/views/modals/main.blade.php
+++ application/views/modals/main.blade.php
@@ -66,7 +66,7 @@
 <div class="modal fade" id="view-ride" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header visible-xs">
+            <div class="modal-header visible-xs" style="border-bottom:0px;">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
             <div class="modal-body"> 
