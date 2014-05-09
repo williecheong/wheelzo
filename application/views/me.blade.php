31318d90e98414e3e5ee57c0818ddc2be8c998cf
williecheong <cheongwillie@hotmail.com>
2014-05-08 22:15:16 -0400

    another grammar

--- application/views/me.blade.php
+++ application/views/me.blade.php
@@ -93,7 +93,7 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     <h4 class="modal-title" id="myModalLabel">
-                        Ride Request - Invitations
+                        Ride Request and Invitations
                     </h4>
                 </div>
                 <div class="modal-body">
