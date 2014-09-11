3e145687dc4f398a5c83668be76fa649d786ea86
williecheong <cheongwillie@hotmail.com>
2014-09-10 23:00:17 -0700

    error catch

--- application/controllers/api/tools.php
+++ application/controllers/api/tools.php
@@ -8,34 +8,35 @@ class Tools extends REST_Controller {
 
         $fb_group_html = isset($data['fb_group_html']) 
                             ? $data['fb_group_html'] 
-                            : $this->load->file("sample.txt", true);
+                            : '' ;
         
+        if ( $fb_group_html == '' ) {
+            $fb_group_html = $this->load->file("sample.txt", true);
+        }
+
         $user_messages = array(); 
-        if ( $fb_group_html !== '' ) {
-            
-            $this->load->library('html_dom');
-            $this->html_dom->loadHTML( $fb_group_html );
-            
-            $postings = $this->html_dom->find('div.mbm');
+        $this->load->library('html_dom');
+        $this->html_dom->loadHTML( $fb_group_html );
+        
+        $postings = $this->html_dom->find('div.mbm');
 
-            foreach( $postings as $posting ) {
-                $posting_data = json_decode($posting->getAttr('data-ft'), true);
-                //var_dump($posting_data);
-                //echo "\n\n\n";
-                if ( isset($posting_data['author']) ) {
-                    $author_fb_id = $posting_data['author'];
-                    $message = $posting->find('div.userContent div.userContent', 0);
-                    try {
-                        $message_content = strip_tags( $message->innertext );
-                        $message_content = htmlspecialchars_decode( $message_content );
+        foreach( $postings as $posting ) {
+            $posting_data = json_decode($posting->getAttr('data-ft'), true);
+            //var_dump($posting_data);
+            //echo "\n\n\n";
+            if ( isset($posting_data['author']) ) {
+                $author_fb_id = $posting_data['author'];
+                $message = $posting->find('div.userContent div.userContent', 0);
+                try {
+                    $message_content = strip_tags( $message->innertext );
+                    $message_content = htmlspecialchars_decode( $message_content );
 
-                        $user_messages[] = array(
-                            "facebook_id" => $author_fb_id,
-                            "raw_message" => $message_content
-                        );
-                    } catch ( Exception $e ) {
-                        // skip and do nothing
-                    }
+                    $user_messages[] = array(
+                        "facebook_id" => $author_fb_id,
+                        "raw_message" => $message_content
+                    );
+                } catch ( Exception $e ) {
+                    // skip and do nothing
                 }
             }
         }
