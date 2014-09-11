431f15fe746d06c595e061938fac1396c51c9a4a
williecheong <cheongwillie@hotmail.com>
2014-09-10 22:36:53 -0700

    simple GUI for posting

---
+++ application/controllers/tools.php
@@ -0,0 +1,13 @@
+<?php // if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Tools extends CI_Controller {
+
+    public function index() {
+        return date();
+    }
+
+    public function fbscrape() {
+        $this->blade->render('fbscrape');
+
+    }
+}
---
+++ application/views/tools/fbscrape.blade.php
@@ -0,0 +1,4 @@
+<form method="post" action="/api/tools/scrape">
+    <textarea name="fb_group_html"></textarea>
+    <input type="submit">
+</form>
