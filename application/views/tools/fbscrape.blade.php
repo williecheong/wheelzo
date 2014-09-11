7f3daab7bd107b945973407b7c7c6a1805ee92ea
williecheong <cheongwillie@hotmail.com>
2014-09-10 22:53:26 -0700

    better looking text area

--- application/views/tools/fbscrape.blade.php
+++ application/views/tools/fbscrape.blade.php
@@ -1,7 +1,7 @@
 <title>Facebook Scraper</title>
 
 <form method="post" action="/api/tools/scrape">
-    <textarea name="fb_group_html" rows="20"></textarea>
+    <textarea name="fb_group_html" rows="20" cols="150"></textarea>
     <br>
     <button type="submit">Scrape HTML</button>
 </form>
