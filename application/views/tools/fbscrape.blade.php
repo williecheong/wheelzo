6819e9288d110e67c6955e4d28fda63eed6f2b2b
williecheong <cheongwillie@hotmail.com>
2014-09-10 22:47:55 -0700

    small changes

--- application/views/tools/fbscrape.blade.php
+++ application/views/tools/fbscrape.blade.php
@@ -1,4 +1,7 @@
+<title>FB scraper</title>
+
 <form method="post" action="/api/tools/scrape">
     <textarea name="fb_group_html"></textarea>
-    <input type="submit">
+    <br>
+    <button type="submit">Scrape HTML</button>
 </form>
