9c3b5a04a401a77f6ad76a62e26d4eee3bd07cef
Willie <cheongwillie@gmail.com>
2015-08-08 06:12:58 -0400

    locking down new version to logged in people

--- application/controllers/v2.php
+++ application/controllers/v2.php
@@ -48,6 +48,10 @@ class V2 extends CI_Controller {
     }
 
     public function index() {
+        if ($this->wheelzo_user_id == false) {
+            redirect( base_url() );
+        }
+
         $this->blade->render('v2/main',
             array(
                 'requested_ride' => $this->ride->retrieve_by_id($this->input->get('ride')),
