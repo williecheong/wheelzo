c3ac546a43c4f8dba6ae22b18b0c3bec2cb8f2f6
Willie <cheongwillie@gmail.com>
2015-08-08 06:04:22 -0400

    attempt to seduce nerds to use v2 from v1

--- application/views/v1/main.blade.php
+++ application/views/v1/main.blade.php
@@ -15,9 +15,14 @@
 @endsection
 
 @section('jumbotron')
-    <div class="jumbotron" id="introduction">
+    <div class="jumbotron" id="introduction-container">
         <div class="container">
-            <div class="row">
+            <a href="/v2" class="btn btn-block btn-default">
+                <i class="fa fa-star"></i>
+                Use the new version 
+                <span class="hidden-xs">that is over v{{ CURRENT_VERSION }}</span>
+            </a>
+            <div class="row" id="introduction">
                 <div class="col-sm-3">
                     <h3>
                         <i class="fa fa-shield fa-2x" style="color:forestgreen;"></i> Assurance
