362104583f20ca43b20a060bd93b2aee1150f9dc
Willie <cheongwillie@gmail.com>
2015-08-19 16:13:12 -0400

    highway for facebook image

--- application/views/v2/lookup.blade.php
+++ application/views/v2/lookup.blade.php
@@ -9,10 +9,6 @@
 
 @section('description')
     <meta name="description" content="Better rideshare and carpooling for people around Kitchener, Waterloo and the Greater Toronto Area">
-    <link rel="image_src"  href="/assets/img/screenshot-lookup_200x200.png">
-    <meta property="og:image" content="{{ base_url() }}assets/img/screenshot-lookup_200x200.png"/>
-    <meta property="og:image:width" content="200" />
-    <meta property="og:image:height" content="200" />
 @endsection
 
 @section('custom_css')
--- application/views/v2/privacy.blade.php
+++ application/views/v2/privacy.blade.php
@@ -9,10 +9,6 @@
 
 @section('description')
     <meta name="description" content="Privacy policies at Wheelzo">
-    <link rel="image_src"  href="/assets/img/screenshot-main_200x200.png">
-    <meta property="og:image" content="{{ base_url() }}assets/img/screenshot-main_200x200.png"/>
-    <meta property="og:image:width" content="200" />
-    <meta property="og:image:height" content="200" />
 @endsection
 
 @section('custom_css')
