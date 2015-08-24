f3a3826052a2ba8c4c6d250ee96176e620040c73
Willie <cheongwillie@gmail.com>
2015-08-24 04:00:59 -0400

    select placeholder directive

--- assets/js/v2/base.js
+++ assets/js/v2/base.js
@@ -272,8 +272,18 @@ app.controller('myController', function ($scope, $sce, $http, $filter, $modal, t
                 }
             });
         }
+    };  
+}).directive('select', function($interpolate) {
+    return {
+        restrict: 'E',
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ctrl) {
+            var defaultOptionTemplate;
+            scope.defaultOptionText = attrs.defaultOption || 'Select...';
+            defaultOptionTemplate = '<option value="" disabled selected style="display: none;">{{defaultOptionText}}</option>';
+            elem.prepend($interpolate(defaultOptionTemplate)(scope));
+        }
     };
-    
 });
 
 
